03055bbd85d8b42ca86cc49091b1dfb6619da352
Simon Brigode <simonbrigode@saucisson-bis.home>
2025-08-27 15:02:28 +0200

    chore: ignore dist (Netlify builds)

--- dist/chunks/2025-08-18-entretien-capitaine_BUxwMuIr.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./2025-08-18-entretien-capitaine_Chty3Wa0.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
--- dist/chunks/astro_D_MpU7R0.mjs
+++
@@ -1,3 +0,0 @@
-import 'kleur/colors';
-import './astro/server_55BT9lWN.mjs';
-import 'clsx';
